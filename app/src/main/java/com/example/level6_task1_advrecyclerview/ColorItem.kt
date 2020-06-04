48bbe62247aa974e93a3b12b7e63850e4f90e821
Leander van Grinsven <leander550@live.nl>
2020-06-04 20:54:52 +0200

    Step 2 + 3

---
+++ app/src/main/java/com/example/level6_task1_advrecyclerview/ColorItem.kt
@@ -0,0 +1,8 @@
+package com.example.level6_task1_advrecyclerview
+
+data class ColorItem(
+    var hex: String,
+    var name: String
+) {
+    fun getImageUrl() = "http://singlecolorimage.com/get/$hex/1080x1080"
+}
