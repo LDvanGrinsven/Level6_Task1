0b2c24f8c65f1e38df01c09c2dd9354c4c631d71
Leander van Grinsven <leander550@live.nl>
2020-06-04 20:42:35 +0200

    Step 1

---
+++ app/src/main/java/com/example/level6_task1_advrecyclerview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.level6_task1_advrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
